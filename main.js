b9c5effd973377348e62dc6d1452b8b7a159bc04
MPA-TEC <laignier.tec@hotmail.com>
2021-11-16 23:58:37 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,99 @@
+/* ====== Abre e fecha o Menu ====== */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+/* ====== laço de condição ====== */
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+/* ====== esconde o menu ao clicar em um item ====== */
+const links = document.querySelectorAll('nav ul li a')
+/* ====== laço de condição ====== */
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+/* ====== Testimonials "Carrocel, Slider" Biblioteca - "Swiper" ====== */
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 'auto',
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+/* ====== Scrollreview : Mostrar elementos quando der scroll na página ====== */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links
+  footer .brand, footer .social
+  `,
+  { interval: 100 }
+)
+/* ====== Button back to top page ====== */
+const backToTopButton = document.querySelector('.back-to-top')
+function buttonBackToTopPage() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+/* ====== Sombreamento no header da pagina durante o scoll ====== */
+/* ====== Shading in the page header during the scroll ====== */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+function headerShadowWhenUsingScroll() {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+}
+/* ===== Activate menu on the page ===== */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+/* ====== When to use on scroll on the page ====== */
+window.addEventListener('scroll', function () {
+  headerShadowWhenUsingScroll()
+  buttonBackToTopPage()
+  activateMenuAtCurrentSection()
+})
